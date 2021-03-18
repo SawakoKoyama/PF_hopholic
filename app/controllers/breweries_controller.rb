81126629c0bbd2f0ea1afc8f8d2974e93a19b76a
EC2 Default User <ec2-user@ip-172-31-10-8.ap-northeast-1.compute.internal>
2021-03-18 13:35:42 +0000

    [Add]投稿機能、投稿一覧

--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -1,5 +1,6 @@
 class BreweriesController < ApplicationController
   def index
+    @breweries = Brewery.all
   end
 
   def show
@@ -9,16 +10,25 @@ class BreweriesController < ApplicationController
   end
 
   def new
+    @brewery = Brewery.new
   end
-  
+
   def create
+    @brewery = Brewery.new(brewery_params)
+    #@brewery.user_id = current_user.id
+    @brewery.save
+    redirect_to breweries_path
   end
-  
+
   def update
   end
-  
+
   def destroy
   end
 
-end
+  private
+  def brewery_params
+    params.require(:brewery).permit(:brewery_name, :image, :prefecture_id, :address, :star, :comment, :user_id)
+  end
 
+end
--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,2 +1,4 @@
 class Brewery < ApplicationRecord
+    belongs_to :user
+    attachment :image
 end
