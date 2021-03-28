dd7c28d4d9f8a87dcdff0b9f26f70af42b6aa627
EC2 Default User <ec2-user@ip-172-31-10-8.ap-northeast-1.compute.internal>
2021-03-28 10:04:57 +0000

    [update]いいね機能に非同期設定追加

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,18 +1,16 @@
 class FavoritesController < ApplicationController
   def create
-    brewery = Brewery.find(params[:brewery_id])
-    favorite = current_user.favorites.new(brewery_id: brewery.id)
+    @brewery = Brewery.find(params[:format])
+    favorite = current_user.favorites.new(brewery_id: @brewery.id)
     favorite.save
-    redirect_to brewery_path(brewery)
+    #redirect_to brewery_path(brewery)
   end
 
   def destroy
-    brewery = Brewery.find(params[:brewery_id])
-    favorite = current_user.favorites.find_by(brewery_id: brewery.id)
+    @brewery = Brewery.find(params[:format])
+    favorite = current_user.favorites.find_by(brewery_id: @brewery.id)
     favorite.destroy
-    redirect_to brewery_path(brewery)
+    #redirect_to brewery_path(brewery)
   end
-
-
 
 end
