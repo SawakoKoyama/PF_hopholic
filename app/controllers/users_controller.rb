03c67cad7b48c275827ef63655f9795cfb9c51ab
EC2 Default User <ec2-user@ip-172-31-10-8.ap-northeast-1.compute.internal>
2021-04-07 13:14:56 +0000

    [update]レイアウトの調整

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @breweries = @user.breweries
+    @breweries = @user.breweries.all.page(params[:page]).per(9)
   end
 
   def edit
